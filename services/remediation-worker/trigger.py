b0ab82a52b34428fe87524dcee7948985ef5a4f8
CENGTOKSOY <gaffartoksoy@hotmail.com>
2026-04-06 19:57:17 +0300

    initial commit

--- services/remediation-worker/trigger.py
+++ services/remediation-worker/trigger.py
@@ -1,17 +1,17 @@
 import boto3
 import json
 
-# LocalStack SQS Bağlantısı
+
 sqs = boto3.client('sqs', region_name="eu-central-1", endpoint_url="http://127.0.0.1:4566", aws_access_key_id="test", aws_secret_access_key="test")
 
 queue_url = sqs.get_queue_url(QueueName="security-alerts-queue")['QueueUrl']
 
-# Fırlatılacak Olay (Event) Verisi
+
 event_payload = {
     "bucket_name": "company-confidential-data"
 }
 
-# Mesajı Kuyruğa Gönder
+
 sqs.send_message(
     QueueUrl=queue_url,
     MessageBody=json.dumps(event_payload)
